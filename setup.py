afef1c4f6c0c8c01bdd2792e45cfa0ce61512a54
Jonne Saleva <jonne.saleva@gmail.com>
2018-08-05 23:10:59 -0600

    fix setup.py to install properly with pip 18.0

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import find_packages, setup
 
-from pip.req import parse_requirements
-
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 
 def get_requirements(filename):
     try:
-        from pip.download import PipSession
-
+        from pip._internal.download import PipSession
         session = PipSession()
+
     except ImportError:
         session = None
 
     reqs = parse_requirements(filename, session=session)
 
     return [str(r.req) for r in reqs]
-
 
 setup_args = dict(
     name='flask-letsencrypt',
