5abe2954fd31c5c6c3931182e6ac34db7abb7932
Nick Joyce <nick.joyce@realkinetic.com>
2018-08-07 11:59:43 +0100

    Update to version 0.2

--- setup.py
+++ setup.py
@@ -5,26 +5,27 @@ try:
 except ImportError:
     from pip.req import parse_requirements
 
+
 def get_requirements(filename):
     try:
         from pip._internal.download import PipSession
         session = PipSession()
-
     except ImportError:
         from pip.download import PipSession
         session = PipSession()
-    except:
+    except Exception:
         session = None
 
     reqs = parse_requirements(filename, session=session)
 
     return [str(r.req) for r in reqs]
 
+
 setup_args = dict(
     name='flask-letsencrypt',
-    version='0.1',
+    version='0.2',
     maintainer='Nick Joyce',
-    maintainer_email='nick@boxdesign.co.uk',
+    maintainer_email='nick.joyce@realkinetic.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt'),
     tests_require=get_requirements('requirements_dev.txt'),
